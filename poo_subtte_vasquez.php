57de0e7dede7f9e2487504c89d18a4e8e7d0505b
Vasquez <cvasquez857@gmail.com>
2024-05-02 21:44:51 -0600

    finalizado

--- poo_subtte_vasquez.php
+++ poo_subtte_vasquez.php
@@ -17,7 +17,7 @@
     
     // Definir Metodo
     public function MostrarProducto(){
-        echo "El producto es: <b>".$this->nombre."</b> y su precio es de: <b>".$this->precio;
+        echo "El producto es: <b>".$this->nombre."</b> y su precio es de: <b>".$this->precio."</b>";
     }
  }
 
@@ -25,3 +25,16 @@
  $producto1 = new Producto('tableta', 200, true);
 //  mostrar Producto
 $producto1->MostrarProducto();
+
+echo "<pre>";
+var_dump($producto1);
+echo "</pre>";
+
+// producto 2
+
+$producto2 = new Producto("Monitor Curvo", "300", true);
+$producto2->MostrarProducto();
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
